499c3d2ac5c810bdcd7e2acd2ac1ffa254981ae9
Juan Manuel Imbachí G <juan.imbachi@correo.icesi.edu.co>
2020-04-21 18:16:22 -0500

    Proyect v.1.0

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify,{
+    customProperties: true,
+    iconfont: 'md'})
+    
+export default new Vuetify()
